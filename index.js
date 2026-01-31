c39795e8ccdb8d177397b7e37cffd4f447991610
Anup Nautiyal <anupn8083@gmail.com>
2026-01-31 23:08:28 +0530

    Index file update

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import pg from "pg";
 const app = express();
 const port = 3000;
 
-const db = new db.Client({
+const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "world",
@@ -13,12 +13,21 @@ const db = new db.Client({
   port: 5432
 });
 
+db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  
-});
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(result.rows);
+  res.render("index.ejs", { countries: countries, total: countries.length });
+  db.end();
+  });
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
