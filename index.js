409e7a69203bfdaf0b8316b71140dc9f117fcd32
Anup Nautiyal <anupn8083@gmail.com>
2026-01-31 22:49:17 +0530

    Index file

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new db.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "anup@1902",
+  port: 5432
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  
 });
 
 app.listen(port, () => {
